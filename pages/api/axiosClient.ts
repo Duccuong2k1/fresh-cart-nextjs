ca8a87497a4544c5995db97a059b000dc2af1264
Cuong Do <doduccuong2001@gmail.com>
2023-04-28 10:57:08 +0700

    Update: link data vercel

--- pages/api/axiosClient.ts
+++ pages/api/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://database-vercel.vercel.app/api",
     headers: {
         "Content-Type": "application/json"
     }
